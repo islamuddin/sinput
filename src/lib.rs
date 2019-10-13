6649dcac7edc11a31c081c424e8353162db2cb7f
ID <uddinislam46@gmail.com>
2019-10-13 10:42:00 +0500

    added implementation of all type of functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,156 @@
 
 #[doc="created function to reuse and reduce LOC for f32 input."]
-#[warn(dead_code)]
+#[allow(dead_code)]
 pub fn as_f32(message:&str)->(f32){
 println!("{}",message);
 let mut input = String::new();
 std::io::stdin().read_line(&mut input).expect("Input Failed");
-let integer: f32 = input.trim().parse().unwrap();
+let float: f32 = input.trim().parse().unwrap();
+float
+}
+
+#[doc="created function to reuse and reduce LOC for i32 input."]
+#[allow(dead_code)]
+pub fn as_i32(message:&str)->(i32){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: i32 = input.trim().parse().unwrap();
 integer
 }
+
+
+
+#[doc="created function to reuse and reduce LOC for string input."]
+#[allow(dead_code)]
+pub fn as_string(message:&str)->(String){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+input
+}
+
+
+#[doc="created function to reuse and reduce LOC for isize input."]
+#[allow(dead_code)]
+pub fn as_isize(message:&str)->(isize){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: isize = input.trim().parse().unwrap();
+integer
+}
+
+
+#[doc="created function to reuse and reduce LOC for usize input."]
+#[allow(dead_code)]
+pub fn as_usize(message:&str)->(usize){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: usize = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for u8 input."]
+#[allow(dead_code)]
+pub fn as_u8(message:&str)->(u8){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: u8 = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for u16 input."]
+#[allow(dead_code)]
+pub fn as_u16(message:&str)->(u16){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: u16 = input.trim().parse().unwrap();
+integer
+}
+
+
+#[doc="created function to reuse and reduce LOC for i8 input."]
+#[allow(dead_code)]
+pub fn as_i8(message:&str)->(i8){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: i8 = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for i16 input."]
+#[allow(dead_code)]
+pub fn as_i16(message:&str)->(i16){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: i16 = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for u32 input."]
+#[allow(dead_code)]
+pub fn as_u32(message:&str)->(u32){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: u32 = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for f64 input."]
+pub fn as_f64(message:&str)->(f64){
+#[allow(dead_code)]
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: f64 = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for i64 input."]
+#[allow(dead_code)]
+pub fn as_i64(message:&str)->(i64){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: i64 = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for u64 input."]
+#[allow(dead_code)]
+pub fn as_u64(message:&str)->(u64){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: u64 = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for u16 input."]
+#[allow(dead_code)]
+pub fn as_i128(message:&str)->(i128){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: i128 = input.trim().parse().unwrap();
+integer
+}
+
+#[doc="created function to reuse and reduce LOC for u16 input."]
+pub fn as_u128(message:&str)->(u128){
+println!("{}",message);
+let mut input = String::new();
+std::io::stdin().read_line(&mut input).expect("Input Failed");
+let integer: u128 = input.trim().parse().unwrap();
+integer
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod lib;
 
 fn main(){
-    let input_f32=lib::as_f32("Enter your f32 input:");
-    println!("f32 input + 1 = {}",input_f32+1.0);
+    let input=lib::as_i8("Enter i8 type input");
+    println!("{}",input);
+
+
 }
